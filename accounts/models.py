c2b8346b21a1d19d8958cd9a76f068242c52913d
HanamiGeto <tidu11@hotmail.de>
2024-06-17 21:58:56 +0200

    added custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    groups = models.ManyToManyField(
+            'auth.Group',
+            related_name='accounts_user_set',
+            related_query_name='accounts_user',
+            blank=True
+        )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='accounts_user_set',
+        related_query_name='accounts_user',
+        blank=True
+    )
