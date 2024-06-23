74eb121c64863a2fedfa0a34316008c053ce3009
HanamiGeto <tidu11@hotmail.de>
2024-06-23 19:22:23 +0200

    changed tasks convert video function to create all needed resolutions

--- content/tasks.py
+++ content/tasks.py
@@ -1,6 +1,13 @@
 import subprocess
 
-def convert_480p(source):
-    target = source[0:len(source) - 4] + '_480p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
-    subprocess.run(cmd)
+def convert_videos_to_resolutions(source):
+    resolutions = {
+        '_1080p': 'hd1080',
+        '_720p': 'hd720',
+        '_360p': '640x360'
+    }
+    for key, value in resolutions.items():
+        target = source[0:len(source) - 4] + '{}.mp4'.format(key)
+        cmd = 'ffmpeg -i "{}" -s {} -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, value, target)
+        subprocess.run(cmd)
+        
