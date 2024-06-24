5bab6386ecc1712b24327c37cfc86ee4d3a756f2
HanamiGeto <tidu11@hotmail.de>
2024-06-24 21:19:53 +0200

    added content url for video views

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('content/', include('content.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('django-rq/', include('django_rq.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
