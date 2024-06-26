8c3520cdca4a2d9e481b2cd4beecb81c0d9a7f4f
HanamiGeto <tidu11@hotmail.de>
2024-06-26 20:35:40 +0200

    updated video serializer to return all resolutions from uploaded video

--- content/serializers.py
+++ content/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Video
 
 class VideoSerializer(serializers.ModelSerializer):
+    video_file_resolutions = serializers.SerializerMethodField()
+
     class Meta:
         model = Video
         fields = '__all__'
+
+    def get_video_file_resolutions(self, obj):
+        resolutions = ['1080p', '720p', '360p']
+        return {
+            res: f'/media/videos/{obj.title.lower()}_{res}.mp4' for res in resolutions
+        }
