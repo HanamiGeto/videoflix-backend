25d02da2ec7ea989311501a7775a2506c8f6bf7c
HanamiGeto <tidu11@hotmail.de>
2024-08-10 17:45:24 +0200

    feat: update video deletion logic to handle HLS .m3u8 and .ts files

--- content/signals.py
+++ content/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import os
 import django_rq
+import glob
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
@@ -19,21 +20,29 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     when corresponding `Video` object is deleted.
     Also deletes all resolution versions of the video.
     """
+    
     if instance.video_file:
         file_path = instance.video_file.path
         if os.path.isfile(file_path):
             os.remove(file_path)
 
-        resolutions = ['_1080p', '_720p', '_360p']
+        # Handle HLS files
+        base_name, _ = os.path.splitext(file_path)
+        hls_files_pattern = f'{base_name}_*.m3u8'
+        ts_files_pattern = f'{base_name}_*.ts'
 
-        base_name, extension = os.path.splitext(file_path)
+        # Delete the .m3u8 files
+        for hls_file in glob.glob(hls_files_pattern):
+            if os.path.isfile(hls_file):
+                os.remove(hls_file)
 
-        for resolution in resolutions:
-            resolution_file_path = f'{base_name}{resolution}{extension}'
-            if os.path.isfile(resolution_file_path):
-                os.remove(resolution_file_path)
+        # Delete the .ts files
+        for ts_file in glob.glob(ts_files_pattern):
+            if os.path.isfile(ts_file):
+                os.remove(ts_file)
 
+    # Remove the thumbnail if it exists
     if instance.thumbnail_file:
-        file_path = instance.thumbnail_file.path
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+        thumbnail_path = instance.thumbnail_file.path
+        if os.path.isfile(thumbnail_path):
+            os.remove(thumbnail_path)
