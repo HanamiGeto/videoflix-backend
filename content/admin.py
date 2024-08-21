9847e7ae97bb4cbddb559c1e6562b1cf09afa9cc
HanamiGeto <tidu11@hotmail.de>
2024-08-21 19:22:26 +0200

    chore: register UserVideoList model in admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Video
+from .models import UserVideoList, Video
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
@@ -9,5 +9,14 @@ class VideoResource(resources.ModelResource):
         model = Video
 
 @admin.register(Video)
+class VideoAdmin(ImportExportModelAdmin):
+    pass
+
+class UserVideoListResource(resources.ModelResource):
+
+    class Meta:
+        model = UserVideoList
+
+@admin.register(UserVideoList)
 class VideoAdmin(ImportExportModelAdmin):
     pass
