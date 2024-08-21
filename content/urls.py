bbdfc56b81d9ca51ad8cd9ab9fe1fa921c5f7d9d
HanamiGeto <tidu11@hotmail.de>
2024-08-21 19:24:48 +0200

    feat: add URL pattern for UserVideoListView

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VideoDetail, VideoList
+from .views import UserVideoListView, VideoDetail, VideoList
 
 urlpatterns = [
     path('videos/', VideoList.as_view(), name='video-list'),
     path('videos/<int:pk>/', VideoDetail.as_view(), name='video-detail'),
+    path('my-videos/', UserVideoListView.as_view(), name='user-video-list')
 ]
